57714244825d0b04c1f0a9f1c01c601a3883f167
TxnUkeGrl <txnukegrl@gmail.com>
2021-07-12 11:30:51 -0400

    full commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreLab23.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreLab23.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        private readonly MovieDBContext _context;
+
+        public MovieController(MovieDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        //Create, Read, Update, Delete (C.R.U.D.) Actions
+
+    }
+}
